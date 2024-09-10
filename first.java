1e5a0b197f29a4d79f7cf1c9ac1f3927f0174a97
Owen40 <omuthami27@gmail.com>
2024-09-10 10:04:43 +0300

    First class

---
+++ first.java
@@ -0,0 +1,6 @@
+class MyFirstJavaClass {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.printf("Hello \n Welcome to Java");
+    }
+}
